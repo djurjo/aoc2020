3a2889d2d3c4e757f463839a5de19d21a18a37e4
djurjo <67223622+djurjo@users.noreply.github.com>
2020-12-01 17:54:37 +0100

    Add files via upload

--- day1/day1.hs
+++ day1/day1.hs
@@ -3,14 +3,18 @@ module Day1 where
 import System.IO
 
 
-alg :: [Int] -> Maybe [Int]
-alg [] = Nothing
-alg l  = Just [ x*y*z | x <- l,
+alg2 :: [Int] -> Maybe [Int]
+alg2 [] = Nothing
+alg2 l  = Just [ x*y*z | x <- l,
                       y <- l,
                       z <- l, 
                       x + y + z == 2020]
 
-
+alg1 :: [Int] -> Maybe [Int]
+alg1 [] = Nothing
+alg1 l  = Just [ x*y | x <- l,
+                      y <- l, 
+                      x + y == 2020]
     
 stringtoInt :: String -> [[Int]]
 stringtoInt s = map (map readInt )(map words (lines s))
@@ -22,11 +26,14 @@ getInput s = map f (stringtoInt s)
     where
       f [a] = a
 
-output :: String -> Maybe [Int]
-output s = alg (getInput s)
+--output :: ([Int] -> Maybe [Int]) ->[Int] -> Maybe [Int]
+--output alg l = alg l
               
 main = do
   contents <- readFile "input.txt"
-  print $ output  contents
- 
+  let
+      inputl = getInput contents
+  print $ alg1 inputl
+  print $ alg2 inputl
+
     
